dd029d0aa4fe4edc824753d931a786b419e8df79
Visethboti <visethboti@gmail.com>
2020-06-20 17:13:40 +0700

    Implemented timer

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     // variables
     int a, b, answer, correctAnswer, totalAnswer, secondLeft;
+    boolean allowGame;
     int[] choiceArray = {0,0,0,0};
+
+    ////////// Functional Methods //////////////
 
     public boolean hasInChoiceArray(int target, int index){
         for(int i = 0; i < 4; i++){
@@ -29,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
+
+    public String formatTime(int s){
+        if(s<10)
+            return s/60 + ":0" + s%60;
+        else
+            return s/60 + ":" + s%60;
+    }
+
+    ///////////////////////////////////////////
 
     public void newProblem(){
         a = (int) Math.ceil(Math.random()*30);
@@ -45,10 +58,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void updateDisplay(){
+        //display problem
         problemView.setText(a+" + "+b);
-
+        //display score
         scoreView.setText(correctAnswer+"/"+totalAnswer);
+        //display time
+        timeView.setText(formatTime(secondLeft));
 
+        //display answers
         button1.setText(Integer.toString(choiceArray[0]));
         button2.setText(Integer.toString(choiceArray[1]));
         button3.setText(Integer.toString(choiceArray[2]));
@@ -58,27 +75,57 @@ public class MainActivity extends AppCompatActivity {
     public void pickedChoice(View view) {
         Log.i("Button", "It is working");
 
-        // Check if selected answer is correct or not, and score them
-        int index = Integer.parseInt(view.getTag().toString());
-        if(choiceArray[index] == answer)
-            correctAnswer += 1;
-        totalAnswer += 1;
+        // Only allow to pick when game session
+        if(allowGame) {
 
-        // Generate new problem
-        newProblem();
-        updateDisplay();
+            // Check if selected answer is correct or not, and score them
+            int index = Integer.parseInt(view.getTag().toString());
+            if (choiceArray[index] == answer)
+                correctAnswer += 1;
+            totalAnswer += 1;
+
+            // Generate new problem
+            newProblem();
+            updateDisplay();
+        }
     }
 
     public void reset() {
         correctAnswer = 0;
         totalAnswer = 0;
-        secondLeft = 0;
+        secondLeft = 30;
+        allowGame = true;
         newProblem();
         updateDisplay();
     }
 
     public void restartButtonPressed(View view){
         reset();
+    }
+
+    public void startTimer(){
+        final Handler handler = new Handler();
+
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                //When time runs out
+                if(secondLeft == 0) {
+                    allowGame = false;
+                    
+                }
+
+                if(secondLeft >= 0 && allowGame) {
+                    //update time display
+                    timeView.setText(formatTime(secondLeft));
+                    secondLeft -= 1;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        // start the runnable function
+        handler.post(run);
     }
 
     @Override
@@ -99,5 +146,6 @@ public class MainActivity extends AppCompatActivity {
 
         //Initizial Variables and new problem
         reset();
+        startTimer();
     }
 }
